c15b7d5c699a32ecd598b267ad2aaf189b815ff1
imaikosuke <imaikosuke2@gmail.com>
2024-07-19 16:31:20 +0900

    Azure SQL DatabaseからAzure Database for PostgreSQLに移行

--- src/azure_sql.py
+++ src/azure_sql.py
@@ -1,35 +1,28 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, DateTime
-from sqlalchemy.sql import insert
-from datetime import datetime
+import psycopg2
+from get_conn import get_connection_uri
 
-# Azure SQL Database　へのデータ追加を行うクラス
-class AzureSQLDatabase:
-    def __init__(self, server, database, username, password):
-        driver = '{ODBC Driver 17 for SQL Server}'
-        connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
-        self.engine = create_engine(connection_string)
-        self.metadata = MetaData(self.engine)
-        self.conditions = Table('conditions', self.metadata,
-                                Column('id', Integer, primary_key=True, autoincrement=True),
-                                Column('moisture', Integer),
-                                Column('timestamp', DateTime))
-        self.photos = Table('photos', self.metadata,
-                            Column('photo_id', Integer, primary_key=True, autoincrement=True),
-                            Column('photo_url', String),
-                            Column('timestamp', DateTime))
+class PostgreSQLDatabase:
+    def __init__(self):
+        conn_string = get_connection_uri()
+        self.conn = psycopg2.connect(conn_string)
+        self.cur = self.conn.cursor()
 
-    # 土壌湿度を追加するメソッド
-    def insert_condition(self, moisture):
-        timestamp = datetime.now()
-        query = insert(self.conditions).values(moisture=moisture, timestamp=timestamp)
-        with self.engine.connect() as connection:
-            connection.execute(query)
-        print(f"Inserted condition: moisture={moisture}, timestamp={timestamp}")
+    def insert_condition(self, moisture: int):
+        try:
+            self.cur.execute("INSERT INTO conditions (moisture) VALUES (%s)", (moisture,))
+            self.conn.commit()
+            print("Inserted condition successfully.")
+        except Exception as e:
+            print(f"Error inserting data: {e}")
 
-    # 写真を追加するメソッド
-    def insert_photo(self, photo_url):
-        timestamp = datetime.now()
-        query = insert(self.photos).values(photo_url=photo_url, timestamp=timestamp)
-        with self.engine.connect() as connection:
-            connection.execute(query)
-        print(f"Inserted photo: photo_url={photo_url}, timestamp={timestamp}")
+    def insert_photo(self, photo_url: str):
+        try:
+            self.cur.execute("INSERT INTO photos (photo_url) VALUES (%s)", (photo_url,))
+            self.conn.commit()
+            print("Inserted photo successfully.")
+        except Exception as e:
+            print(f"Error inserting data: {e}")
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
