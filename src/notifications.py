de0ef4719e7506c3de89a034c6dba4ae85af98e1
imaikosuke <imaikosuke2@gmail.com>
2024-07-25 12:27:20 +0900

    センサ値によるLINEの通知をRaspbeery Piから直接行うように修正

--- src/notifications.py
+++ src/notifications.py
@@ -5,16 +5,21 @@ import random
 
 load_dotenv()
 
-LINE_API_URL = os.getenv("LINE_API_URL")
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_ID = os.getenv("LINE_CHANNEL_ID")
 
 def send_line_water_notification():
     water_messages = ["喉乾いたぁ", "水ほしいな", "もうカラカラだよ～", "水ちょーだい！", "水！", "水やりまだ～？"]
     message = water_messages[random.randint(0, 5)]
     headers = {
         'Content-Type': 'application/json',
+        'Authorization': f'Bearer {LINE_CHANNEL_ACCESS_TOKEN}'
     }
-    payload = {"message": message}
-    response = requests.post(LINE_API_URL, headers=headers, json=payload)
+    push_message = {
+        "to": LINE_CHANNEL_ID,
+        "messages": [{"type": "text", "text": message}]
+    }
+    response = requests.post('https://api.line.me/v2/bot/message/push', headers=headers, json=push_message)
     if response.status_code != 200:
         print(f"Error sending notification: {response.status_code}, {response.text}")
 
@@ -23,8 +28,12 @@ def send_line_brightness_notification():
     message = brightness_messages[random.randint(0, 5)]
     headers = {
         'Content-Type': 'application/json',
+        'Authorization': f'Bearer {LINE_CHANNEL_ACCESS_TOKEN}'
     }
-    payload = {"message": message}
-    response = requests.post(LINE_API_URL, headers=headers, json=payload)
+    push_message = {
+        "to": LINE_CHANNEL_ID,
+        "messages": [{"type": "text", "text": message}]
+    }
+    response = requests.post('https://api.line.me/v2/bot/message/push', headers=headers, json=push_message)
     if response.status_code != 200:
         print(f"Error sending notification: {response.status_code}, {response.text}")
